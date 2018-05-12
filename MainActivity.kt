d5f1492180e79d3099e454bb31621e16a4c9fab3
Kuppa <rajkuppa@gmail.com>
2018-05-12 12:52:09 +0530

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -10,11 +10,19 @@ import android.os.Environment
 import android.support.v4.content.ContextCompat
 import android.view.View
 import android.widget.Toast
+import com.kuppa.web3samplekotlin.R.id.CreateWalletBtn
 import kotlinx.android.synthetic.main.activity_main.*
+import org.slf4j.LoggerFactory
 import org.web3j.crypto.CipherException
+import org.web3j.crypto.Credentials
 import org.web3j.crypto.WalletUtils
+import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.methods.response.TransactionReceipt
+import org.web3j.tx.Transfer
+import org.web3j.utils.Convert
 import java.io.File
 import java.io.IOException
+import java.math.BigDecimal
 import java.security.InvalidAlgorithmParameterException
 import java.security.NoSuchAlgorithmException
 import java.security.NoSuchProviderException
@@ -22,6 +30,9 @@ import java.security.NoSuchProviderException
 class MainActivity : AppCompatActivity() {
 
     var newFile1 = null
+    private val log = LoggerFactory.getLogger(MainActivity::class.java)
+    private var web3: Web3j? = null
+    private var credentials: Credentials? = null
 
     @SuppressLint("SdCardPath")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +50,38 @@ class MainActivity : AppCompatActivity() {
         walletPathFile.mkdirs()
 
         CreateWalletBtn.setOnClickListener {
-            val walletPath = WalletUtils.generateFullNewWalletFile("12345", walletPathFile)
+            val walletPath = WalletUtils.generateFullNewWalletFile("Your own password", walletPathFile)
             Toast.makeText(this, "Wallet created successfully.", Toast.LENGTH_SHORT).show()
+        }
+
+        connectEthereumClientBtn.setOnClickListener {
+            val web3ClientVersion = web3!!.web3ClientVersion().sendAsync().get()
+            val clientVersion = web3ClientVersion.getWeb3ClientVersion()
+            log.info("Connected to Ethereum client version: $clientVersion")
+        }
+
+        loadCredentialsBtn.setOnClickListener {
+            credentials = WalletUtils.loadCredentials(
+                    "Your wallet password",
+                    "Your wallet stored path")
+            log.info("Credentials loaded Address is::: " + credentials!!.getAddress())
+        }
+
+        transferFundsBtn.setOnClickListener {
+            var transactionReceipt: TransactionReceipt? = null
+            try {
+                log.info("Sending 1 Wei ("
+                        + Convert.fromWei("1", Convert.Unit.ETHER).toPlainString() + " Ether)")
+
+                transactionReceipt = Transfer.sendFunds(
+                        web3, credentials, "0x19e03255f667bdfd50a32722df860b1eeaf4d635",
+                        BigDecimal.valueOf(1.0), Convert.Unit.ETHER)
+                        .send()
+                log.info("Transaction complete, view it at https://rinkeby.etherscan.io/tx/" + transactionReceipt!!.blockHash)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
         }
     }
 
